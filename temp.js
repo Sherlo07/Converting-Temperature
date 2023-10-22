54c99b7d36fe0e28ccc50664733660096f5f4ecd
irfan <148051540+Sherlo07@users.noreply.github.com>
2023-10-22 14:54:48 +0530

    Add files via upload

---
+++ temp.js
@@ -0,0 +1,33 @@
+document.getElementById("submitButton").onclick=function(){
+      let temp;
+      if(document.getElementById("cButton").checked){
+        temp=document.getElementById("textBox").value;
+        temp=Number(temp);
+        temp = toCelsius(temp);
+        document.getElementById("tempLabel").innerHTML=temp +"°C";
+
+      }
+      else if(document.getElementById("fButton").checked){
+        temp=document.getElementById("textBox").value;
+        temp=Number(temp);
+        temp = toFahreheit(temp);
+        document.getElementById("tempLabel").innerHTML=temp +"°F";
+
+
+      }
+      else{
+        document.getElementById("tempLabel").innerHTML="Select a Unit";
+
+      }
+
+       function toCelsius(temp)
+       {
+        return (temp-32) * (5/9);
+        }
+        function toFahreheit(temp)
+        {
+            return temp* 9/5 +32;
+        }
+
+
+}
